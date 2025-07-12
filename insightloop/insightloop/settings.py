47fe397cdc4fa6ad20f4dfd2daac3e2ea61fa836
KRISHNA GOYAL <krishnagoyal@KRISHNAs-MacBook-Air-2.local>
2025-07-12 18:59:39 +0530

    ready for deployment

--- insightloop/insightloop/settings.py
+++ insightloop/insightloop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9rgdok5ulclnpv(m)5!+*)wg%js%w6hadj^w_#o7iqa_pim24p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
