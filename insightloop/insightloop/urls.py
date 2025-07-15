0246aa2c37f9c9b1aee2124ee41fe78b50ba6b89
KRISHNA GOYAL <krishnagoyal@KRISHNAs-MacBook-Air-2.local>
2025-07-16 01:53:40 +0530

    ready for deployment

--- insightloop/insightloop/urls.py
+++ insightloop/insightloop/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.views import serve as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,4 +16,15 @@ urlpatterns = [
 
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    # Development static files
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Production static file fallback
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', 
+                lambda request, path: static_serve(request, path, insecure=True)),
+    ]
